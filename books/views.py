7d24283e441681735bdfc2fe889df19ecf94b1e2
Didi Ebere <eiweala@live.com>
2021-07-05 12:31:15 +0800

    completed basick setup

--- books/views.py
+++ books/views.py
@@ -1,5 +1,10 @@
+from books.models import Book
 from django.shortcuts import render
 
 # Create your views here.
 def books_index(request):
-    return render(request, "books/index.html")
+
+    books = Book.objects.all()
+    
+    context = { "booksList": books}
+    return render(request, "books/index.html", context)
