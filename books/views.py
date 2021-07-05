57a98aa2f76e8beafa8fb70978aca955953d1367
Didi Ebere <eiweala@live.com>
2021-07-05 12:46:31 +0800

    completed basick setup

--- books/views.py
+++ books/views.py
@@ -7,6 +7,8 @@ def books_index(request):
     form = BookForm()
 
     if request.method == "POST":
+        # files get stored in request.FILES
+        # request.POST stored form body
         form = BookForm(request.POST, request.FILES) 
         if form.is_valid():
             form.save()
